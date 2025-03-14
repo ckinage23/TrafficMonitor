dbc914ee921a0787b2939d3329b66659c41eff83
Chinmayee <chinmayee.kinage@gmail.com>
2025-03-14 21:01:26 +0530

    Change in default url mapping

--- trafficmonitoringapp/trafficmonitoringapp/urls.py
+++ trafficmonitoringapp/trafficmonitoringapp/urls.py
@@ -18,16 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-from ..api.views import index
-from trafficmonitoringapp.trafficmonitoringapp import settings
+from api.views import index
+import trafficmonitoringapp.settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if trafficmonitoringapp.settings.DEBUG:
+    urlpatterns+=static(trafficmonitoringapp.settings.STATIC_URL, document_root=trafficmonitoringapp.settings.STATIC_ROOT)
 
 urlpatterns += [
     re_path(r'^(?:.*)/?$', index, name='index'),
